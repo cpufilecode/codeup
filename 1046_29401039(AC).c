dee580f4ceda28591c3ba4f83486311381ab295f
건우 이 <zf0908@naver.com>
2022-12-01 23:32:23 +0900

    upload

---
+++ 1046_29401039(AC).c
@@ -0,0 +1,18 @@
+﻿
+#include <stdio.h>
+ 
+ 
+int main (void)
+{
+    int a, b, c, ans;
+    float avr;
+    
+    scanf("%d %d %d", &a, &b, &c);
+    ans = a + b + c;
+    avr = (float)ans/3;
+    
+    printf("%d\n",ans);
+    printf("%.01f",avr);
+    
+    return 0;
+}
