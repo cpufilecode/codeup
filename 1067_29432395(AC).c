75aaa4913361f57a04a777dcf82ffc03262c910e
건우 이 <zf0908@naver.com>
2022-12-04 22:51:10 +0900

    upload

--- 1067_29432395(AC).c
+++ 1067_29432395(AC).c
@@ -6,14 +6,14 @@ int main()
 	int a;
 	scanf("%d",&a);
 
-	if(a<0){
+	if(a<0) {
 	    printf("minus\n");
 	    if(a%2==0){
 		    printf("even\n");
 	    } else {
 	        printf("odd\n");
 	    }
-	} else if(a>0){
+	} else if(a>0) {
 	    printf("plus\n");
 	    if (a%2==0) {
 	        printf("even\n");
