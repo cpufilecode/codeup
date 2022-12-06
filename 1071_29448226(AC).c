3dab570b7474abf8826aa2e2efbe5c0ee6b8ed91
건우 이 <zf0908@naver.com>
2022-12-07 00:02:14 +0900

    upload

---
+++ 1071_29448226(AC).c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void){
+    int a;
+    reload:
+    scanf("%d", &a);
+    
+    if(a!=0) {
+        printf("%d\n", a);
+        goto reload;
+    }
+    
+    return 0;
+}
