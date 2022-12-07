ed1e984b5a49e3cbf7f53b78fd3c92502e110c6e
건우 이 <zf0908@naver.com>
2022-12-07 22:38:38 +0900

    upload

---
+++ 1074_29460980(AC).c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n;
+    scanf("%d", &n);
+    
+    while(n!=0)
+    {
+        scanf("%d", &n);
+        printf("%d\n", n);
+        n=n-1;
+    }
+    
+	return 0;
+}
