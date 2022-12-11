6e0a804da5b2cd01ba13160ad5265a9e1c1755b5
건우 이 <zf0908@naver.com>
2022-12-11 23:09:01 +0900

    upload

---
+++ 1080_29504391(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a,sum,i = 0;
+    scanf("%d", &a);
+    
+    while(sum < a) {
+        i++;
+        sum = sum + i;
+    }
+    printf("%d", i);
+    
+	return 0;
+}
