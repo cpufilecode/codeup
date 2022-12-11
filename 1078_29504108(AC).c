885366447aea33619521e51f780bbf461d31d1a4
건우 이 <zf0908@naver.com>
2022-12-11 22:38:47 +0900

    upload

---
+++ 1078_29504108(AC).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int sum=0;
+    int i, n;
+    scanf("%d", &n);
+    
+    for(i=1; i<=n; i++)
+    {
+        if(i%2==0) 
+            sum=sum+i;
+            
+    }
+    printf("%d\n", sum);
+
+	return 0;
+}
