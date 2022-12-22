fa62ebcb008e42395ebe4afe3a5710a355804d04
건우 이 <zf0908@naver.com>
2022-12-22 13:23:53 +0900

    upload

---
+++ 1086_29616694(AC).c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	long long int w,h,b;
+    double MB;
+    
+    scanf("%lld %lld %lld", &w, &h, &b );
+    MB = (w*h*b);
+    
+    printf("%.02lf MB",MB/(8*1024*1024));
+    
+	return 0;
+}
